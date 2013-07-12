215c950c5f34f0b2bede1ff63d58fd92f0231bf8
shaikjaveed <ipapstudent@iPap010s-iMac.local>
2013-07-12 08:51:42 +0530

    Initial Commit

---
+++ TableExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableExample
+//
+//  Created by ipapstudent on 12/07/13.
+//  Copyright (c) 2013 ipapstudent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableExample
+//
+//  Created by ipapstudent on 12/07/13.
+//  Copyright (c) 2013 ipapstudent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
